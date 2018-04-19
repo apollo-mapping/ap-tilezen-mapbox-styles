d294a90cb6e45c24642b9adebedfb2422e12f361
Maddie Abboud <maddie.abboud@gmail.com>
2018-04-18 22:52:30 -0600

    fix toner highway label color

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 s3Bucket=$1
 awsProfile=$2
 
-aws s3 sync sprites s3://${s3Bucket}/sprites --acl public-read --profile ${awsProfile}
+aws s3 sync sprites s3://${s3Bucket}/sprites --acl public-read --cache-control no-cache --profile ${awsProfile}
