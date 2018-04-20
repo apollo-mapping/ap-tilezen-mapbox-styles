a3093b3b1b7db5977c6589c3e76a188b71373644
Maddie Abboud <maddie.abboud@gmail.com>
2018-04-20 17:41:28 -0600

    add demo page and remove toner icons

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+const FlowWebpackPlugin = require('flow-webpack-plugin')
+
+let libraryName = pkg.name;
+
+let plugins = [
+    new FlowWebpackPlugin(),
+];
+
+let outputFile;
+
+if (env === 'build') {
+    plugins.push(new UglifyJsPlugin({minimize: true}));
+    outputFile = libraryName + '.min.js';
+} else {
+    outputFile = libraryName + '.js';
+}
+
+const config = {
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/lib',
+        filename: outputFile,
+        library: "apolloStyles",
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /^node_modules/,
+                loader: 'babel-loader',
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.resolve('./node_modules'), path.resolve('./src')],
+        extensions: ['.json', '.js']
+    },
+    plugins: plugins
+};
+
+module.exports = config;
