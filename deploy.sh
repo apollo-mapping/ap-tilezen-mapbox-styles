b1c671b4f8b347e1a86f315c588cb9f6e7819153
Maddie Abboud <maddie.abboud@gmail.com>
2018-04-23 10:06:27 -0600

    neighbourhood labels now that I have wof data in

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,7 @@ s3Bucket=$1
 awsProfile=$2
 
 aws s3 sync sprites s3://${s3Bucket}/sprites --acl public-read --cache-control no-cache --profile ${awsProfile}
+
+# upload built js file for demo that uses plain old js <script> tags
+npm run build-webpack
+aws s3 sync lib/@apollomapping s3://osm-vector-tiles-apollomapping/style --acl public-read --cache-control no-cache --profile ${awsProfile}
