4b0c5187eaefa76639ff4074af93446d438b755b
Maddie Abboud <maddie.abboud@gmail.com>
2018-07-27 17:32:05 -0600

    add support for multi xyz tile url templating

--- src/StyleFactory.js
+++ src/StyleFactory.js
@@ -32,23 +32,16 @@ export class StyleFactory {
 
     createStyle(style: {}) {
         var styleString = JSON.stringify(style);
+
         // real lazy way to set json template variables convert to string replace then back to json
         // creating styles is something you only do once on app start up so no performance worries
         styleString = styleString.replace(/{resource_url}/g, this.resourceUrl);
-        styleString = styleString.replace(/{tile_server_xyz_url}/g, this.tileUrl);
 
+        const vectorUrl = this.createTileUrl(this.tileUrl);
+        styleString = styleString.replace(/\["{tile_server_xyz_url}"]/g, vectorUrl);
 
-        // for seperate a./b./c. tile service urls that are used to get around
-        // browser max request limit mapbox won't do this itself for some reason
-        if (this.satelliteRasterUrl && this.satelliteRasterUrl.includes("{s}")) {
-            const tileUrls = [];
-            for (let serverNumOn of ["a", "b", "c"])
-                tileUrls.push(this.satelliteRasterUrl.replace("{s}", serverNumOn));
-
-            styleString = styleString.replace(/\["{satellite_raster_xyz_url}"]/g, JSON.stringify(tileUrls));
-        }
-        else
-            styleString = styleString.replace(/{satellite_raster_xyz_url}/g, this.satelliteRasterUrl);
+        const rasterUrl = this.createTileUrl(this.satelliteRasterUrl);
+        styleString = styleString.replace(/\["{satellite_raster_xyz_url}"]/g, rasterUrl);
 
         var localizationField = "{name}";
         if (this.localization && this.localization.length > 0)
@@ -57,5 +50,20 @@ export class StyleFactory {
         styleString = styleString.replace(/{localization_name}/g, localizationField);
 
         return JSON.parse(styleString);
+    }
+
+    createTileUrl(baseTileUrl: string) {
+        // for seperate a./b./c. tile service urls that are used to get around
+        // browser max request limit mapbox won't do this itself for some reason unless
+        // a special mapbox style url
+        if (baseTileUrl && baseTileUrl.includes("{s}")) {
+            const tileUrls = [];
+            for (let serverNumOn of ["a", "b", "c"])
+                tileUrls.push(baseTileUrl.replace("{s}", serverNumOn));
+
+            return JSON.stringify(tileUrls);
+        }
+
+        return "[\"" + baseTileUrl + "\"]";
     }
 }
