301428fc7c6e01d7cff7d027f2de7aec9972b01f
Omer <eldaromer@gmail.com>
2018-10-25 14:23:14 -0600

    Added polyfill for string.prototype.includes

--- src/StyleFactory.js
+++ src/StyleFactory.js
@@ -22,6 +22,22 @@ if(!String.prototype.endsWith) {
     }
 }
 
+if (!String.prototype.includes) {
+    Object.defineProperty(String.prototype, 'includes', {
+        value: function (search, start) {
+            if (typeof start !== 'number') {
+                start = 0
+            }
+
+            if (start + search.length > this.length) {
+                return false
+            } else {
+                return this.indexOf(search, start) !== -1
+            }
+        }
+    })
+}
+
 export class StyleFactory {
     tileUrl: string = "";
     resourceUrl: string = "";
